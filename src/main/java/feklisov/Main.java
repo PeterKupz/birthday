7aac2d298b08d9fb8e6c946e1e0108497e961c36
PeterKupz <petr.kuptcov@yandex.ru>
2020-09-13 23:46:28 +0300

    Создал одну станицу, подключил шаблонизатор и стили

---
+++ src/main/java/feklisov/Main.java
@@ -0,0 +1,11 @@
+package feklisov;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Main extends SpringApplication{
+	public static void main(String[] args) {
+		Main.run(Main.class, args);
+	}
+}
---
+++ src/main/java/feklisov/MainController.java
@@ -0,0 +1,16 @@
+package feklisov;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class MainController {
+	
+	
+	@GetMapping({"", "index"})
+	String index(Model model) {
+		return "index";
+	}
+	
+}
